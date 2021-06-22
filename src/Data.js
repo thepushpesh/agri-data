3bd0cf5041c6d4a0a517250dfe9e2bfd83dc7637
Pushpesh Sharma <pushpeshsharmaa@gmail.com>
2021-06-22 15:52:47 +0530

    added dummy data

---
+++ src/Data.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import {IoMdOpen} from 'react-icons/io';
+
+
+export const Data =
+            [
+                {
+                    id: 1,
+                    name: "Seeds/Seedlings",
+                    image: "./image/seed.jpg",
+                    description: "Chih Sakata 651 (1500 seeds)",
+                    quantity: 26,
+                    icons: <IoMdOpen/>,
+                },
+                {
+                    id: 2,
+                    name: "Nutrient/Fertilizer",
+                    image: "./image/fertilizer.jpg",
+                    description: "Acme Brand Fert A/B Set - 50Kg",
+                    quantity: 114,
+                    icons: <IoMdOpen/>,
+                },
+                {
+                    id: 3,
+                    name: "Pesticide/Fungicide",
+                    image: "./image/pesticide.png",
+                    description: "Valiants ME (Methyl Engeidl) 30ml",
+                    quantity: 40,
+                    icons: <IoMdOpen/>,
+                },
+                {
+                    id: 4,
+                    name: "Growing Medium",
+                    image: "./image/growing.jpg",
+                    description: "Acme Brand Cocohusk - 20kg",
+                    quantity: 20,
+                    icons: <IoMdOpen/>,
+                },
+                {
+                    id: 5,
+                    name: "Growing Medium",
+                    image: "./image/grow.jpg",
+                    description: "Acme Brand Cocohusk - 50kg",
+                    quantity: 50,
+                    icons: <IoMdOpen/>,
+                },
+                {
+                    id: 6,
+                    name: "Pesticide/Fungicide",
+                    image: "./image/pesticide.png",
+                    description: "Decis (Delthamatrin) 1L",
+                    quantity: 4,
+                    icons: <IoMdOpen/>,
+                },
+            ]
+        
