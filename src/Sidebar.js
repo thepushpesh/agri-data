fc1bd9a15370394858405de85fce643cabcdd989
Pushpesh Sharma <pushpeshsharmaa@gmail.com>
2021-06-22 15:52:29 +0530

    added sidebar detail

---
+++ src/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Sidebar() {
+    return (
+        <aside className="side-nav">
+            <ul>
+                <li><p>Agri-Purchase Manangement</p></li>
+                <li><p>Agri-Input Master Maintenance</p></li>
+                <li><p>Supplier Master Maintenance</p></li>
+                <li><p>Agri-Input Purchase Request</p></li>
+            </ul>
+            <p className='extra'>Tax Master Maintenance</p>
+        </aside>
+        )
+}
